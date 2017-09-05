63bf0b943bfa29f2bf0096a5faacdda5185120cc
MrDj200 <mrdj201@gmail.com>
2017-09-05 13:12:08 +0200

    Initial

--- src/main/java/me/dj/myTest/common/CommonProxy.java
+++ src/main/java/me/dj/myTest/common/CommonProxy.java
@@ -2,9 +2,11 @@ package me.dj.myTest.common;
 
 import me.dj.myTest.blocks.FirstBlock;
 import me.dj.myTest.blocks.ModBlocks;
+import me.dj.myTest.utils.LogHelper;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,26 +22,29 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber
 public class CommonProxy 
 {
-    public void preInit(FMLPreInitializationEvent e) 
+    public void preInit(FMLPreInitializationEvent event) 
     {
+        
     }
     
-    public void init(FMLInitializationEvent e) 
+    public void init(FMLInitializationEvent event) 
     {
         MinecraftForge.EVENT_BUS.register(new EventHandlerCommon());
     }
     
-    public void postInit(FMLPostInitializationEvent e) 
+    public void postInit(FMLPostInitializationEvent event) 
     {
     }
     
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
         event.getRegistry().register(new FirstBlock());
+        LogHelper.info("Registered new Block: FirstBlock");
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().register(new ItemBlock(ModBlocks.firstBlock).setRegistryName(ModBlocks.firstBlock.getRegistryName()));
     }
+    
 }
